4184f78fb7d537636c0675479f601e6781115871
A04pr <areed041704@gmail.com>
2024-02-13 20:17:18 -0700

    apollo

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,57 @@
+const { User } = require('./models'); 
+const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require('apollo-server-express');
+
+const resolvers = {
+  Query: {
+    me: async (_, args, context) => {
+      if (!context.user) {
+        throw new AuthenticationError('You must be logged in to view this information');
+      }
+      return await User.findById(context.user._id);
+    }
+  },
+  Mutation: {
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('Invalid email or password');
+      }
+      const correctPassword = await user.isCorrectPassword(password);
+      if (!correctPassword) {
+        throw new AuthenticationError('Invalid email or password');
+      }
+      const token = jwt.sign({ _id: user._id }, 'your-secret-key', { expiresIn: '1h' });
+      return { token, user };
+    },
+    addUser: async (_, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = jwt.sign({ _id: user._id }, 'your-secret-key', { expiresIn: '1h' });
+      return { token, user };
+    },
+    saveBook: async (_, { bookId, title, authors, description, image, link }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError('You must be logged in to save a book');
+      }
+      const user = await User.findByIdAndUpdate(
+        context.user._id,
+        { $addToSet: { savedBooks: { bookId, title, authors, description, image, link } } },
+        { new: true }
+      );
+      return user;
+    },
+    removeBook: async (_, { bookId }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError('You must be logged in to remove a book');
+      }
+      const user = await User.findByIdAndUpdate(
+        context.user._id,
+        { $pull: { savedBooks: { bookId } } },
+        { new: true }
+      );
+      return user;
+    }
+  }
+};
+
+module.exports = resolvers;
