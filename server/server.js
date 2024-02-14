a903bc503a069611e1c81e1642cc5d23bb873f36
A04pr <areed041704@gmail.com>
2024-02-13 20:03:13 -0700

    init

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers'); 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,7 +12,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Initialize Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Apply Apollo middleware to Express app
+server.applyMiddleware({ app });
+
+// If we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
