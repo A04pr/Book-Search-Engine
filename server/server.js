bbc2c85fa91e20ce06aed99877b72e33216643bd
A04pr <areed041704@gmail.com>
2024-02-13 20:31:13 -0700

    auth

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers'); 
+const resolvers = require('./resolvers');
+const { authMiddleware } = require('./auth'); 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,16 +13,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Initialize Apollo Server
+app.use(authMiddleware);
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// Apply Apollo middleware to Express app
 server.applyMiddleware({ app });
 
-// If we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
